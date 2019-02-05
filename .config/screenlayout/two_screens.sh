7c5460efe5e7f66627a9b842058891da99d03ad8
flaport <floris.laporte@gmail.com>
2019-02-05 14:19:18 +0100

    seperated screenlayout from i3 config

---
+++ .config/screenlayout/two_screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-0 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
