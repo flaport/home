9754339cd2e3961d2b0bd58681eff98526c69f8b
flaport <floris.laporte@gmail.com>
2019-02-07 13:51:04 +0100

    added qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,2 @@
+c.url.default_page = "https://raw.githubusercontent.com/qutebrowser/qutebrowser/master/doc/img/cheatsheet-big.png"
+c.url.start_pages = "https://raw.githubusercontent.com/qutebrowser/qutebrowser/master/doc/img/cheatsheet-big.png"
