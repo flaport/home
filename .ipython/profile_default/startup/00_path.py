445bb059a5e61f881dc7fc0dd30c0eb2c83e3ebd
flaport <floris.laporte@gmail.com>
2019-03-01 11:22:39 +0100

    small bugfix in ipython path script

--- .ipython/profile_default/startup/00_path.py
+++ .ipython/profile_default/startup/00_path.py
@@ -8,4 +8,5 @@ with open(__expand('~/.pythonpath'), 'r') as __file:
     __pth = __file.readlines()
 for __p in __pth:
     __path.insert(0, __expand(__p.strip()))
+__path.insert(0, "")
 del __expand, __path, __file, __pth
