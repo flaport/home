a4e66310d3d2cd8ab7ce3be5095396cff16b96ea
flaport <floris.laporte@gmail.com>
2019-06-03 10:33:30 +0200

    used xsetroot to create status bar

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
-sxhkd -c ~/.config/sxhkd/sxhkdrcdwm
+sxhkd -c ~/.config/sxhkd/sxhkdrcdwm &
+
+dte(){
+  dte="$(date +"%A, %B %d | 🕒 %l:%M%p")"
+  echo -e "$dte"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "🖪 $mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "💻 $cpu% cpu"
+}
+
+while true; do
+     xsetroot -name "$(cpu) | $(mem) | $(dte)"
+     sleep 10s    # Update time every ten seconds
+done &
+
