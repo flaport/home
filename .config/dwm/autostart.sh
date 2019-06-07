7aad374c18a7d1fd38f5bebe41dc992b7492c378
flaport <floris.laporte@gmail.com>
2019-06-07 11:08:09 +0200

    added dwm autostart script

---
+++ .config/dwm/autostart.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#    _____ _     ____  ____  ____  ____  _____
+#   /    // \   /  _ \/  __\/  _ \/  __\/__ __\
+#   |  __\| |   | / \||  \/|| / \||  \/|  / \
+#   | |   | |_/\| |-|||  __/| \_/||    /  | |
+#   \_/   \____/\_/ \|\_/   \____/\_/\_\  \_/
+
+sxhkd -c ~/.config/sxhkd/sxhkdrcdwm &
+
+dte(){
+  dte="$(date +"%A, %B %d | 🕒 %l:%M%p")"
+  echo -e "$dte"
+}
+
+mem(){
+  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "🖪 $mem"
+}
+
+cpu(){
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "💻 $cpu% cpu"
+}
+
+while true; do
+     xsetroot -name "$(cpu) | $(mem) | $(dte)"
+     sleep 10s    # Update time every ten seconds
+done &
+
