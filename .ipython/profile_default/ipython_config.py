7495da8227616d65e508ee77c1990b012596cc1f
flaport <floris.laporte@gmail.com>
2019-12-02 15:43:24 +0100

    added ipython script

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -254,13 +254,13 @@ c.InteractiveShell.pdb = False
 #c.InteractiveShell.quiet = False
 
 ##
-#c.InteractiveShell.separate_in = ''
+c.InteractiveShell.separate_in = ''
 
 ##
-#c.InteractiveShell.separate_out = ''
+c.InteractiveShell.separate_out = ''
 
 ##
-#c.InteractiveShell.separate_out2 = ''
+c.InteractiveShell.separate_out2 = ''
 
 ## Show rewritten input, e.g. for autocall.
 c.InteractiveShell.show_rewritten_input = False
@@ -290,6 +290,21 @@ c.TerminalInteractiveShell.confirm_exit = False
 #c.TerminalInteractiveShell.display_completions = 'multicolumn'
 
 ## Shortcut style to use at the prompt. 'vi' or 'emacs'.
+import sys
+from prompt_toolkit.key_binding.vi_state import InputMode, ViState
+def get_input_mode(self):
+    return self._input_mode
+def set_input_mode(self, mode):
+    shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 4)
+    raw = u'\x1b[{} q'.format(shape)
+    if hasattr(sys.stdout, '_cli'):
+        out = sys.stdout._cli.output.write_raw
+    else:
+        out = sys.stdout.write
+    out(raw)
+    sys.stdout.flush()
+    self._input_mode = mode
+ViState.input_mode = property(get_input_mode, set_input_mode)
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
 ## Set the editor used by IPython (default to $EDITOR/vi/notepad).
