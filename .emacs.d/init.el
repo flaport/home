87e7b3c8d4f427a630aac4fa0b79be6c78a75b09
flaport <floris.laporte@gmail.com>
2019-12-04 14:40:12 +0100

    udpated opacity rules

---
+++ .emacs.d/init.el
@@ -0,0 +1,4 @@
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+;;(set-frame-parameter (selected-frame) 'alpha <both>)
+(set-frame-parameter (selected-frame) 'alpha '(90 . 90))
+(add-to-list 'default-frame-alist '(alpha . (90 . 90)))
