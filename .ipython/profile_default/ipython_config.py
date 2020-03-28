0ebe6ea9cbebdc475e4a6786384cf4b3a668902e
flaport <floris.laporte@gmail.com>
2020-03-28 09:02:51 +0100

    reduce timeoutlen for ESC in ipython

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -293,17 +293,22 @@ c.TerminalInteractiveShell.confirm_exit = False
 import sys
 from prompt_toolkit.key_binding.vi_state import InputMode, ViState
 def get_input_mode(self):
+    if sys.version_info[0] == 3:  # decrease delay of ESC button
+        from prompt_toolkit.application.current import get_app
+        app = get_app()
+        app.timeoutlen = 0
     return self._input_mode
 def set_input_mode(self, mode):
     shape = {InputMode.NAVIGATION: 1, InputMode.REPLACE: 3}.get(mode, 4)
     raw = u'\x1b[{} q'.format(shape)
     if hasattr(sys.stdout, '_cli'):
-        out = sys.stdout._cli.output.write_raw
+        write = sys.stdout._cli.output.write_raw
     else:
-        out = sys.stdout.write
-    out(raw)
+        write = sys.stdout.write
+    write(raw)
     sys.stdout.flush()
     self._input_mode = mode
+ViState._input_mode = InputMode.INSERT
 ViState.input_mode = property(get_input_mode, set_input_mode)
 c.TerminalInteractiveShell.editing_mode = 'vi'
 
